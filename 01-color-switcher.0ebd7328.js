1271a492b77d76a46143e9cf23238d30746be6f1
Valerii219 <119235568+Valerii219@users.noreply.github.com>
2023-05-14 20:11:50 +0000

    Deploying to gh-pages from @ Valerii219/goit-js-hw-09@534da93757736c8e68cca3a1d2d86d8ad8dc84b0 🚀

--- 01-color-switcher.0ebd7328.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.body.style.backgroundColor="";const o=t.start.addEventListener("click",(()=>{e=setInterval((()=>{console.log(t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`)}),1e3),!0!==o&&t.start.setAttribute("disabled",!0)})),r=t.stop.addEventListener("click",(()=>{clearInterval(e),!0!==r&&t.start.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.0ebd7328.js.map
