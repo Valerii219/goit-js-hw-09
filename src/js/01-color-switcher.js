ec7b374825a6580c32e0e1fd837188a86e8e8260
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-14 20:10:00 +0200

    1 task;

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,28 +3,37 @@ start: document.querySelector('button[data-start]'),
 stop: document.querySelector('button[data-stop]'),
 body: document.querySelector('body')
 };
+
 let timerId = null;
 
-refs.body.style.backgroundColor = getRandomHexColor();
-refs.start.addEventListener('click', () => {
-    
+refs.body.style.backgroundColor = "";
 
+const clickStart = refs.start.addEventListener('click', () => {
+    timer();
+    buttonStart();
+})
+
+const clickStop = refs.stop.addEventListener('click', () =>{
+    clearInterval(timerId);
+    buttonStop();
+}
+)
+
+function timer(){
     timerId = setInterval(() => {
-        console.log();
-    },1000)
-    
-
+        console.log(refs.body.style.backgroundColor = getRandomHexColor());
+    }, 1000)
 }
-)
 
-refs.stop.addEventListener('click', () =>{
-
+function buttonStart(){
+    if(clickStart !== true)
+    {refs.start.setAttribute('disabled', true)}
 }
-)
-
-
-
-
+    function buttonStop(){
+        if(clickStop !== true){
+            refs.start.removeAttribute('disabled');
+        }
+    }
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
