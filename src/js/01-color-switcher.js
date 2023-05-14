505b0cfb67a45de25c7457ed2cd38fbb5eca414c
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-14 19:05:42 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+start: document.querySelector('button[data-start]'),
+stop: document.querySelector('button[data-stop]'),
+body: document.querySelector('body')
+};
+let timerId = null;
 
+refs.body.style.backgroundColor = getRandomHexColor();
+refs.start.addEventListener('click', () => {
+    
+
+    timerId = setInterval(() => {
+        console.log();
+    },1000)
+    
+
+}
+)
+
+refs.stop.addEventListener('click', () =>{
+
+}
+)
+
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
