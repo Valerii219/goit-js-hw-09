9871483c92ddf1ca4fdcb924b0464690b141e4e3
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-16 22:36:44 +0200

    2 half +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,12 +2,69 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const buttonStart = document.querySelector('button[data-start]');
+
+const dayEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+const disabledButton = buttonStart.setAttribute('disabled', true);
 
 const dataFlatOpt = flatpickr("#datetime-picker", {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-        onClose(selectedDates){console.log(selectedDates[0])} });
+        onClose(selectedDates){
 
-        // window.alert()
+            if(selectedDates[0] <= Date.now()){
+         window.alert("Please choose a date in the future");
+            }
+            if(selectedDates[0] >= Date.now()) {
+                buttonStart.removeAttribute('disabled');
+            }
+            else{
+                buttonStart.setAttribute('disabled', true);
+            }
+            const startTime = selectedDates[0];
+            const startTimeInSec = startTime.getTime();
+
+            const buttonClick = buttonStart.addEventListener('click', () => {
+                {setInterval(() => {
+                    const currentTime = Date.now();
+                    const deltaTime = startTimeInSec - currentTime;
+                    timer(deltaTime);
+                    }
+                    , 1000);
+                    }
+        });
+    }
+    })
+    
+        function convertMs(ms) {
+            // Number of milliseconds per unit of time
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+            // Remaining days
+            const days = Math.floor(ms / day);
+            // Remaining hours
+            const hours = Math.floor((ms % day) / hour);
+            // Remaining minutes
+            const minutes = Math.floor(((ms % day) % hour) / minute);
+            // Remaining seconds
+            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+            return { days, hours, minutes, seconds }
+            ;} 
+
+            function timer(deltaTime){
+                const{ days, hours, minutes, seconds } = convertMs(deltaTime);
+                dayEl.textContent = days; 
+                hoursEl.textContent = hours ;
+                minutesEl.textContent = minutes;
+                secondsEl.textContent = seconds;
+
+            }
+
