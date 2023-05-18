9f18149a18be9e7e37f632a32aeb7d26c47641af
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-18 19:21:20 +0200

    end 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
 
 const buttonStart = document.querySelector('button[data-start]');
 
@@ -11,60 +11,56 @@ const secondsEl = document.querySelector('[data-seconds]');
 
 const disabledButton = buttonStart.setAttribute('disabled', true);
 
-const dataFlatOpt = flatpickr("#datetime-picker", {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-        onClose(selectedDates){
-
-            if(selectedDates[0] <= Date.now()){
-         window.alert("Please choose a date in the future");
-            }
-            if(selectedDates[0] >= Date.now()) {
-                buttonStart.removeAttribute('disabled');
-            }
-            else{
-                buttonStart.setAttribute('disabled', true);
-            }
-            const startTime = selectedDates[0];
-            const startTimeInSec = startTime.getTime();
-
-            const buttonClick = buttonStart.addEventListener('click', () => {
-                {setInterval(() => {
-                    const currentTime = Date.now();
-                    const deltaTime = startTimeInSec - currentTime;
-                    timer(deltaTime);
-                    }
-                    , 1000);
-                    }
-        });
+const dataFlatOpt = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      window.alert('Please choose a date in the future');
     }
-    })
-    
-        function convertMs(ms) {
-            // Number of milliseconds per unit of time
-            const second = 1000;
-            const minute = second * 60;
-            const hour = minute * 60;
-            const day = hour * 24;
-            // Remaining days
-            const days = Math.floor(ms / day);
-            // Remaining hours
-            const hours = Math.floor((ms % day) / hour);
-            // Remaining minutes
-            const minutes = Math.floor(((ms % day) % hour) / minute);
-            // Remaining seconds
-            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-            return { days, hours, minutes, seconds }
-            ;} 
+    if (selectedDates[0] >= Date.now()) {
+      buttonStart.removeAttribute('disabled');
+    } else {
+      buttonStart.setAttribute('disabled', true);
+    }
+    const startTime = selectedDates[0];
+    const startTimeInSec = startTime.getTime();
 
-            function timer(deltaTime){
-                const{ days, hours, minutes, seconds } = convertMs(deltaTime);
-                dayEl.textContent = days; 
-                hoursEl.textContent = hours ;
-                minutesEl.textContent = minutes;
-                secondsEl.textContent = seconds;
+    const buttonClick = buttonStart.addEventListener('click', () => {
+      {
+        setInterval(() => {
+          const currentTime = Date.now();
+          const deltaTime = startTimeInSec - currentTime;
+          timer(deltaTime);
+        }, 1000);
+      }
+    });
+  },
+});
 
-            }
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
 
+function timer(deltaTime) {
+  const { days, hours, minutes, seconds } = convertMs(deltaTime);
+  dayEl.textContent = `${days.toString().padStart(2, '0')}`;
+  hoursEl.textContent = `${hours.toString().padStart(2, '0')}`;
+  minutesEl.textContent = `${minutes.toString().padStart(2, '0')}`;
+  secondsEl.textContent = `${seconds.toString().padStart(2, '0')}`;
+}
