cf413502bd5012d1a162585b6896ee253d02494d
Valerii219 <119235568+Valerii219@users.noreply.github.com>
2023-05-18 19:53:33 +0000

    Deploying to gh-pages from @ Valerii219/goit-js-hw-09@83712026258bbe2b175792b6ebd93fcc4e0f087d 🚀

---
+++ 03-promises.734cbd1e.js
@@ -0,0 +1,2 @@
+({button:document.querySelector("button")}).button.addEventListener("submit");
+//# sourceMappingURL=03-promises.734cbd1e.js.map
---
+++ 03-promises.e8f63819.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("submit");
+//# sourceMappingURL=03-promises.e8f63819.js.map
