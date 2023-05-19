c3640fbd299a92b5b5286a4683b8505c76253707
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-19 21:49:52 +0200

    121

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,35 @@
+const refs = {
+  button:document.querySelector('button'),
+  delay:document.querySelector('.form input[name = "delay"]'),
+  step:document.querySelector('.form input[name = "step"]'),  
+  amount:document.querySelector('.form input[name = "amount"]') 
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      setTimeout(() => { 
+        if (shouldResolve) {
+      console.log(`Promise ${position} виконано`);
+      resolve();
+      } else {
+        reject(console.log(`Promise ${position}  не виконано`))
+      }}, 
+      delay)})
+      
+  });
+  promise.then(position => {
+    
+    console.log(position);})
+    .catch(delay => {
+      console.log(delay);
+    });
 }
 
-const refs = {
-  button:document.querySelector('button')
-};
-refs.button.addEventListener("submit", )
+
+refs.button.addEventListener("submit", function(e)  {
+  e.preventDefault();
+  console.log(refs.amount.value);
+}) 
+
