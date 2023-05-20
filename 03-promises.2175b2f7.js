e8f251220a65d6cfb443ffbdca8cbfc193dec0ab
Valerii219 <119235568+Valerii219@users.noreply.github.com>
2023-05-20 12:49:24 +0000

    Deploying to gh-pages from @ Valerii219/goit-js-hw-09@bcdd1e10adbe85913d82d7d476bd23b5866c47b6 🚀

--- 03-promises.2175b2f7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={button:document.querySelector("button"),delay:document.querySelector('.form input[name = "delay"]'),step:document.querySelector('.form input[name = "step"]'),amount:document.querySelector('.form input[name = "amount"]')};e.button.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.amount.value)}))}();
-//# sourceMappingURL=03-promises.2175b2f7.js.map
--- 03-promises.9d9010e2.js
+++
@@ -1,2 +0,0 @@
-const e={button:document.querySelector("button"),delay:document.querySelector('.form input[name = "delay"]'),step:document.querySelector('.form input[name = "step"]'),amount:document.querySelector('.form input[name = "amount"]')};e.button.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.amount.value)}));
-//# sourceMappingURL=03-promises.9d9010e2.js.map
